ab31b97557b2cd7914919954c0aa19396f597298
chriskilelo <chriskilelo@gmail.com>
2023-11-15 08:32:02 +0300

    Implement the getArrayDifference(arr1, arr2) function which is responsible for taking in two arrays, comparing them, removing the elements that are common on both arrays and returning a new array that only has the unique elements that were not removed.

--- index.js
+++ index.js
@@ -3,3 +3,18 @@
  * 14-NOV-2023
  * The main entry file
  */
+
+// Require colors to enable us to log to the console in different colors
+require('colors')
+// Require dotenv which loads environment variables from the .env file into process.env
+require('dotenv').config()
+const helpers = require('./src/utils/Helpers')
+
+/**
+ * getArrayDifference(arr1, arr2)
+ * */
+const arr1 = ['a','b','c','d','e','f','g','h'];
+const arr2 = [];
+const result = helpers.getArrayDifference(arr1, arr2);
+console.log(result)
+
--- src/utils/Helpers.js
+++ src/utils/Helpers.js
@@ -4,18 +4,29 @@
  * All utility functions hat should be used across the application will reside on this file
  */
 
+/**
+ * 
+ * @param {*} array1 
+ * @param {*} array2 
+ * @returns 
+ */
 const getArrayDifference = (array1, array2) => {
-    // Remove/filter out the elements in array1 that are present in array2
-    const uniqueArray1Elements = array1.filter(element => !array2.includes(element));
-    // Remove/filter out the elements in array2 that are present in array1
-    const uniqueArray2Elements = array2.filter(element => !array1.includes(element));
-    // Combine the two resultant arrays above that contain unique elements
-    const uniqueElementsArray = uniqueArray1Elements.concat(uniqueArray2Elements);
-    // Return the result to the calling function
-    return uniqueElementsArray;
-
+    // Check if the first array supplied is empty
+    if (array1.length === 0) {
+        // Means the first array is empty, return an empty array
+        return [];
+    } else {
+        // Means the first array has data, therefore remove the elements in array1 that are present in array2
+        const uniqueArray1Elements = array1.filter(element => !array2.includes(element));
+        // Remove/filter out the elements in array2 that are present in array1
+        const uniqueArray2Elements = array2.filter(element => !array1.includes(element));
+        // Combine the two resultant arrays above that contain unique elements
+        const uniqueElementsArray = uniqueArray1Elements.concat(uniqueArray2Elements);
+        // Return the result to the calling function
+        return uniqueElementsArray;
+    }
 }
 
 module.exports = {
-    getArrayDifference
+    getArrayDifference,
 }
